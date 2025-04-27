9d8ec8d981aaf421c672b1fdacf4dd81e0ca8a70
Hicham Errhani <hichamerrihani.pro@gmail.com>
2025-04-27 22:30:57 +0100

    Initial commit - VoteTracker-Pipeline project

--- vote-web-app/app.py
+++ vote-web-app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from kafka import KafkaProducer
+from elasticsearch import Elasticsearch
 import json
 import time
 
 app = Flask(__name__)
 
+# Kafka Producer
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
+
+# Elasticsearch Client
+es = Elasticsearch("http://localhost:9200")
 
 @app.route('/')
 def serve_form():
@@ -21,5 +26,12 @@ def vote():
     producer.send('votes', value=data)
     return "Vote envoyé avec succès !"
 
+@app.route('/last-votes')
+def last_votes():
+    res = es.search(index="votes", size=5, sort="timestamp:desc")
+    votes = [hit["_source"] for hit in res["hits"]["hits"]]
+    return jsonify(votes)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
